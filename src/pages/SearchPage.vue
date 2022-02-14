3a6ba81561cc262cc6c27a485916752c65255b90
n3-rd <godwinjdayo@gmail.com>
2022-02-14 03:56:24 +0100

    corrected columns again

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -19,26 +19,28 @@
         @click="searchPlayer"
       />
 
-      <div class="row">
-        <div
-          v-for="player in players"
-          :key="player.idPlayer"
-          class="players full-width q-pa-md q-mt-md col-sm-12 col-lg-4 col-xl-3 col-md-6"
-          rounded
-        >
-          <q-card class="my-card">
-            <q-img :src="player.strThumb">
-              <div class="absolute-bottom text-h6 player-name">
-                {{ player.strPlayer }}
-              </div>
-            </q-img>
+      <div class="container">
+        <div class="row">
+          <div
+            v-for="player in players"
+            :key="player.idPlayer"
+            class="players q-pa-md q-mt-md col-xs-12 col-lg-4 col-xl-3 col-md-6 col-sm-12"
+            rounded
+          >
+            <q-card class="my-card">
+              <q-img :src="player.strThumb">
+                <div class="absolute-bottom text-h6 player-name">
+                  {{ player.strPlayer }}
+                </div>
+              </q-img>
 
-            <q-card-section class="poppins-medium">
-              Position: {{ player.strPosition }} <br />
-              Club: {{ player.strTeam }} <br />
-              Born: {{ player.dateBorn }} <br />
-            </q-card-section>
-          </q-card>
+              <q-card-section class="poppins-medium">
+                Position: {{ player.strPosition }} <br />
+                Club: {{ player.strTeam }} <br />
+                Born: {{ player.dateBorn }} <br />
+              </q-card-section>
+            </q-card>
+          </div>
         </div>
       </div>
     </div>
